517e23188d2f15f0788e3aed03b7d2102abb47b4
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-11-25 10:44:30 +0100

    plotted random forest

--- random_forest.py
+++ random_forest.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def random_forest(x_train, y_train, x_test, y_test):
@@ -19,5 +21,16 @@ def random_forest(x_train, y_train, x_test, y_test):
 
     rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
     rf_test_r2 = r2_score(y_test, y_rf_test_pred)
+
+    plt.figure(figsize=(5, 5))
+    plt.scatter(x=y_train, y=y_rf_train_pred, c="#7CAE00", alpha=0.3)
+
+    z = np.polyfit(y_train, y_rf_train_pred, 1)
+    p = np.poly1d(z)
+
+    plt.plot(y_train, p(y_train), '#F8766D')
+    plt.ylabel("Predict logS")
+    plt.xlabel("Experimental logS")
+    plt.show()
 
     return pd.DataFrame(["Random Forest", rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
